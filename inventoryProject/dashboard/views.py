c6c3131279fdc02a7ddd22a81c9172da77e4aa8e
Minuri Dahara <minuridahara709@gmail.com>
2024-07-17 11:52:22 +0530

    new response name staff

--- inventoryProject/dashboard/views.py
+++ inventoryProject/dashboard/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
       return HttpResponse("This is the Index Page")
+  
+  
+def staff(request):
+    return HttpResponse('This is the staff page')
